37df6a8a98ee45f8d026b8212fd942475e2e6769
arena <arena@users.noreply.github.com>
2026-04-30 14:20:51 -0700

    Revert to BrowserRouter with basename
    - HashRouter was causing URL issues
    - Will handle sharing URLs in share functionality instead

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 
+const basename = import.meta.env.PROD ? '/cna' : '/';
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HashRouter>
+    <BrowserRouter basename={basename}>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </StrictMode>,
 )
